865d8b70de2cd464949cf4ffea08bbb99f95e188
Raphaël Bacco <raphael@bacco.fr>
2023-05-18 10:54:03 +0200

    Moteur de recherche

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -20,9 +20,24 @@ class SearchController extends AbstractController
     ) {
         $search = $request->query->get('search');
 
-        $chantiers = $em->getRepository(Chantier::class)->findBySearch($search);
-        $references = $em->getRepository(Reference::class)->findBySearch($search);
-        $users = $em->getRepository(User::class)->findBySearch($search);
+        if (strlen($search) < 3) {
+            return $this->redirectToRoute('dashboard');
+        }
+
+        $chantiers = [];
+        if ($this->isGranted('ROLE_CHANTIER_LIST')) {
+            $chantiers = $em->getRepository(Chantier::class)->findBySearch($search);
+        }
+
+        $references = [];
+        if ($this->isGranted('ROLE_REFERENCE_LIST')) {
+            $references = $em->getRepository(Reference::class)->findBySearch($search);
+        }
+
+        $users = [];
+        if ($this->isGranted('ROLE_USER_LIST')) {
+            $users = $em->getRepository(User::class)->findBySearch($search);
+        }
 
         return $this->render('search/index.html.twig', [
             'search' => $search,
