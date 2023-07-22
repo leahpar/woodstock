aed99ce09bd2ddae5567ed5cc14ac4311613b935
Raphaël Bacco <raphael@bacco.fr>
2023-07-22 10:40:43 +0200

    Update bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -2,3 +2,9 @@ update user u
 left join chantier c on c.conducteur_travaux_id = u.id
 set u.conducteur_travaux = 1
 where c.conducteur_travaux_id is not null
+
+-- -----------------------------
+-- Remise à zéro du stock
+-- -----------------------------
+
+delete from panier;
