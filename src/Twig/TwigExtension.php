692e83c0efad752c3e1c3831effd5545ef9a4c67
Raphaël Bacco <raphael@bacco.fr>
2024-02-19 14:28:55 +0100

    recherche détaillée (référence & matériel)

--- src/Twig/TwigExtension.php
+++ src/Twig/TwigExtension.php
@@ -2,7 +2,6 @@
 
 namespace App\Twig;
 
-use App\Entity\Reference;
 use chillerlan\QRCode\QRCode;
 use chillerlan\QRCode\QROptions;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,7 +24,6 @@ class TwigExtension extends AbstractExtension
     {
         return [
             new TwigFilter('number',  [$this, 'number']),
-            new TwigFilter('selectLabel',  [$this, 'selectLabel']),
             new TwigFilter('euro',  [$this, 'euro'], ['is_safe' => ['html']]),
             new TwigFilter('pourcent',  [$this, 'pourcent'], ['is_safe' => ['html']]),
             new TwigFilter('qrcode',  [$this, 'qrcode'], ['is_safe' => ['html']]),
@@ -35,25 +33,9 @@ class TwigExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('getRoles', [$this, 'getRoles']),
-            new TwigFunction('getCategories', [$this, 'getCategories']),
-            new TwigFunction('getSearchOrder', $this->getSearchOrder(...)),
             new TwigFunction('getReferer', $this->getReferer(...)),
+            new TwigFunction('getSearchOrder', $this->getSearchOrder(...)),
         ];
-    }
-
-    public function selectLabel($choices, ?string $value = null): ?string
-    {
-        $res = null;
-        foreach ($choices as $k => $v) {
-            if (is_iterable($v)) {
-                $res = $this->selectLabel($v, $value);
-            }
-            elseif ($v->value == $value) {
-                $res = $v->label;
-            }
-        }
-        return $res;
     }
 
     public function number(?string $value = null, ?string $format = null, ?string $symbol = null): ?string
@@ -86,6 +68,7 @@ class TwigExtension extends AbstractExtension
 
     public function qrCode(?string $value = null): ?string
     {
+        // TODO: remplacer par yaqrgen ?
         $options = [
             'version' => 3, // https://www.qrcode.com/en/about/version.html
             //'versionMin' => 5,
@@ -97,51 +80,6 @@ class TwigExtension extends AbstractExtension
 
         $qrcode = new QRCode(new QROptions($options));
         return $qrcode->render($value);
-    }
-
-    public function getCategories(): array
-    {
-        return array_keys(Reference::CATEGORIES);
-    }
-
-    /**
-     * NB: la liste doit correspondre à Ap\Entity\Role::cases()
-     */
-    public function getRoles(): array
-    {
-        return [
-            'Admin' => [
-                'ROLE_ADMIN' => 'Administrateur (accès à tout)',
-            ],
-            'Compta' => [
-                'ROLE_COMPTA' => 'Exports comptables',
-            ],
-            'Utilisateurs' => [
-                'ROLE_USER_LIST' => 'Consulter les utilisateurs',
-                'ROLE_USER_EDIT' => 'Modifier les utilisateurs',
-            ],
-            'Chantiers' => [
-                'ROLE_CHANTIER_LIST' => 'Consulter les chantiers',
-                'ROLE_CHANTIER_EDIT' => 'Modifier les chantiers',
-            ],
-            'Matériel' => [
-                'ROLE_MATERIEL_LIST' => 'Consulter le matériel',
-                'ROLE_MATERIEL_EDIT' => 'Modifier le matériel',
-            ],
-            'Certificat' => [
-                'ROLE_CERTIFICAT_LIST' => 'Consulter les certificats',
-                'ROLE_CERTIFICAT_EDIT' => 'Modifier les certificats',
-            ],
-            'Stock' => [
-                'ROLE_REFERENCE_LIST' => 'Consulter les références',
-                'ROLE_REFERENCE_EDIT' => 'Modifier les références',
-                'ROLE_REFERENCE_STOCK' => 'Modifier les stocks',
-            ],
-            'Planning' => [
-//                'ROLE_PLANNING_LIST' => 'Consulter le planning',
-                'ROLE_PLANNING_EDIT' => 'Modifier le planning',
-            ],
-        ];
     }
 
     public function getSearchOrder($search, $field): ?string
