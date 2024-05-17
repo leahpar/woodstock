62a5d5fc716ed2715c5861151af27e8fe4d2c5c8
Raphaël Bacco <raphael@bacco.fr>
2024-05-17 15:22:35 +0200

    Logs medias

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -5,13 +5,12 @@ namespace App\Controller;
 use App\Entity\Certificat;
 use App\Entity\Media;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MediaController extends AbstractController
+class MediaController extends CommonController
 {
     #[Route('/medias/upload', name: 'media_upload', methods: ['POST'])]
     public function upload(
@@ -43,7 +42,9 @@ class MediaController extends AbstractController
         $media->certificat = $certificat;
 
         $em->persist($media);
+        $this->log('doc_upload', $certificat, ['document' => $media->originalName]);
         $em->flush();
+
 
         return new Response(null, 201);
     }
@@ -52,6 +53,7 @@ class MediaController extends AbstractController
     public function delete(Request $request, Media $media, EntityManagerInterface $em): Response
     {
         $em->remove($media);
+        $this->log('doc_delete', $media->certificat, ['document' => $media->originalName]);
         $em->flush();
 
         $referer = $request->headers->get('referer');
